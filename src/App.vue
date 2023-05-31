e7476bdd3ae3bbc647404345e194dd9ec40f779f
Jenuel Ganawed <jenuelganawed936@gmail.com>
2023-05-31 16:39:17 +0800

    Updates

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const text = ref("loading");
+
+function openBroJenuel() {
+    window.open("https://brojenuel.com", "_blank");
+}
+
+onMounted(() => {
+    console.log("here");
+
+    setTimeout(() => {
+        text.value = "Done";
+    }, 2000);
+});
+</script>
 
 <template>
-    <div class="min-w-320px">Hello World!</div>
+    <div class="min-w-320px max-w-400px w-full bg-[var(--background)]">
+        <div class="w-full mx-auto flex justify-between items-center p-2">
+            <div
+                @click="openBroJenuel()"
+                class="flex items-center text-[var(--gray-lightest)] cursor-pointer text-size-20px"
+            >
+                <span>Bro</span>
+                <span class="text-[var(--primary)] font-800">Jenuel</span>
+                <span>Articles</span>
+            </div>
+        </div>
+        <div class="pl-2 pr-2 pb-1">Articles {{ text }}</div>
+    </div>
 </template>
