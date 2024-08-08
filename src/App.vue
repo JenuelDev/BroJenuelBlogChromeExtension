fb4b74c4b5a00263b85f90034101dead598d2d04
Jenuel Oras Ganawed <me@jenuel.dev>
2024-08-08 17:14:17 +0800

    Updatese

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@ const DATA = reactive<{
         tags: Array<any>;
         created_at: any;
         blog_meta: any;
+        updated_at: any;
     }>;
 }>({
     blogList: [],
@@ -42,7 +43,7 @@ function commafy(num: number) {
 
 function getArticles() {
     axios
-        .get("https://brojenuel.com/articles.json")
+        .get("https://www.jenuel.dev/articles.json")
         .then(({ data }) => {
             DATA.blogList = data;
         })
@@ -57,103 +58,55 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="w-400px bg-[var(--background)]">
+    <div class="w-400px bg-[var(--background)] mx-auto">
         <div
-            class="w-full mx-auto flex justify-between items-center p-2 fixed bg-[var(--background)] z-50"
+            class="sticky top-0 w-full max-w-800px mx-auto z-9999 bg-white bg-opacity-50 backdrop-blur-md p-3 flex items-center justify-between mx-3 text-center"
         >
-            <div
-                @click="openBroJenuel()"
-                class="flex items-center text-[var(--gray-lightest)] cursor-pointer text-size-20px"
-            >
-                <span>Bro</span>
-                <span class="text-[var(--primary)] font-800">Jenuel</span>
-                <span>Articles</span>
-            </div>
+            <a href="/" class="text-18px font-800">
+                <span>Jenuel</span
+                ><span class="text-[var(--c-blue-500)]">Dev</span>
+            </a>
         </div>
         <div class="pl-2 pr-2 pb-1 pt-10">
             <div class="min-h-[100vh]">
-                <div
-                    class="grid grid-cols-1 gap-3 sm:pl-0 pl-20px"
-                    v-if="DATA.blogList.length"
-                >
-                    <div
+                <div class="flex flex-col gap-10" v-if="DATA.blogList.length">
+                    <a
                         v-for="blog in DATA.blogList"
                         :key="blog.id"
-                        @click="
-                            clickLInk(`https://brojenuel.com/blog/${blog.slug}`)
-                        "
-                        class="p-10px rounded-md transform translate-y-1 hover:translate-y-0 transition-transform cursor-pointer group hover:bg-[var(--background-secondary)] flex flex-col gap-3 ml-4"
+                        :href="`https://www.jenuel.dev/blog/${blog.slug}`"
+                        target="_blank"
+                        class="flex flex-col gap-5 group"
                     >
-                        <div
-                            v-if="
-                                blog.cover_img &&
-                                !(blog.cover_img.indexOf('youtube') > -1)
-                            "
-                            class="rounded-2xl overflow-hidden"
-                        >
-                            <img
-                                :src="blog.cover_img"
-                                class="float-right w-full"
-                            />
+                        <img
+                            v-if="blog.cover_img"
+                            :src="blog.cover_img"
+                            class="rounded-lg w-full mx-auto col-span-2 w-full"
+                            width="300"
+                            height="200"
+                            :alt="blog.title"
+                        />
+                        <div class="content-summary leading-5 col-span-6">
+                            <div class="text-xs sm:mb-1">
+                                {{
+                                    dayjs(blog.updated_at).format(
+                                        "DD MMM, YYYY"
+                                    )
+                                }}
+                            </div>
+                            <h4
+                                class="sm:mb-2 group-hover:text-[var(--c-blue-500)]"
+                            >
+                                {{ blog.title }}
+                            </h4>
+                            <p
+                                :class="`line-clamp-6 text-sm ${
+                                    blog.cover_img && 'line-clamp-3'
+                                }`"
+                            >
+                                {{ blog.summary }}
+                            </p>
                         </div>
-                        <div class="relative group md:order-1">
-                            <div
-                                class="absolute h-10px w-10px bg-gray-600 -left-5 group-hover:bg-[var(--primary)] opacity-50 group-hover:opacity-100 transition-all duration-300 rounded-lg delay top-[50%] transform translate-y-[-50%] translate-x-[-50%]"
-                            ></div>
-                            <div
-                                class="absolute h-1px w-1px group-hover:h-50px bg-[var(--primary)] transition-all duration-500 -left-5 top-[50%] opacity-0 group-hover:opacity-100 transform translate-y-[-50%] translate-x-[-50%]"
-                            ></div>
-                            <div class="absolute top-40%"></div>
-                            <div>
-                                <span
-                                    class="group-hover:text-[var(--primary)] text-size-18px font-800"
-                                >
-                                    {{ blog.title }}.
-                                </span>
-                                <span
-                                    class="opacity-80 font-poly text-size-15px"
-                                    >{{ blog.summary }}</span
-                                >
-                            </div>
-                            <div class="flex gap-2 my-1">
-                                <ul
-                                    class="flex gap-1 flex-wrap text-size-13px p-0"
-                                >
-                                    <li
-                                        v-for="tags in blog.tags"
-                                        :key="tags"
-                                        :class="`tag-${tags}`"
-                                        class="tag"
-                                    >
-                                        #{{ tags }}
-                                    </li>
-                                </ul>
-                            </div>
-                            <div>
-                                <span
-                                    class="italic font-500 opacity-50 whitespace-nowrap flex gap-20px"
-                                >
-                                    {{
-                                        dayjs(blog.created_at).format(
-                                            "MMM. DD, YYYY"
-                                        )
-                                    }}
-                                    <span class="flex items-center gap-7px">
-                                        <Icon
-                                            icon="ic:baseline-remove-red-eye"
-                                        />
-                                        {{
-                                            blog.blog_meta
-                                                ? commafy(
-                                                      blog.blog_meta.view_count
-                                                  )
-                                                : 0
-                                        }}
-                                    </span>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
+                    </a>
                 </div>
                 <div
                     v-show="loading"
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'virtual:uno.css'
-import "./assets/style/main.scss"
+import { createApp } from "vue";
+import App from "./App.vue";
+import '@unocss/reset/tailwind.css'
+import "virtual:uno.css";
+import "./assets/style/main.scss";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
